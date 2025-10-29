d695ab550f1c80e694c420122270c1e88cb7c1e9
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-29 09:32:41 +0900

    10.29(수) _ SQL #18 코드카타

---
+++ SQL_Programmers/String,Date _ DATETIME에서 DATE로 형 변환.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59414
+-- ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 SQL문을 작성해주세요. 이때 결과는 아이디 순으로 조회해야 합니다.
+
+select animal_id, name, date_format(datetime, '%Y-%m-%d') '날짜'
+from animal_ins
+order by 1
