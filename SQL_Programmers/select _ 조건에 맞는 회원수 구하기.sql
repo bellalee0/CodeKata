018537a7dccb65166806e0092463905b3205b124
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-30 09:03:51 +0900

    10.30(목) _ SQL #22 코드카타

---
+++ SQL_Programmers/select _ 조건에 맞는 회원수 구하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131535
+-- USER_INFO 테이블에서 2021년에 가입한 회원 중 나이가 20세 이상 29세 이하인 회원이 몇 명인지 출력하는 SQL문을 작성해주세요.
+
+select count(1) users
+from user_info
+where year(joined)='2021' and age between 20 and 29
