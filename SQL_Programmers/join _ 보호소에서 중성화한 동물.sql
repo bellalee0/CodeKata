3dfd1e510c589946ec7abfbcc40b0f7277baf7a6
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-11 10:18:34 +0900

    11.11(화) _ SQL #36 코드카타

---
+++ SQL_Programmers/join _ 보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59045
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다. 보호소에 들어올 당시에는 중성화1되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+
+select i.animal_id, i.animal_type, i.name
+from animal_ins i inner join animal_outs o on i.animal_id = o.animal_id
+where sex_upon_intake like 'Intact%' and sex_upon_outcome not like 'Intact%'
+order by 1
