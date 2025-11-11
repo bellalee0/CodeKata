c819472f8c9e7482beb6706c34df42525c23fa07
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-11 11:04:26 +0900

    11.11(화) _ 알고리즘 #37 코드카타

---
+++ Algorithm_Programmers/행렬의_덧셈.java
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+// 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+// 제한 조건
+// 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+
+public class 행렬의_덧셈 {
+    public static void main(String[] args) {
+        SumArray sumArray = new SumArray();
+        int[][] arr1 = {{1, 2}, {2, 3}};
+        int[][] arr2 = {{3, 4}, {5, 6}};
+        System.out.println(sumArray.solution(arr1, arr2));
+
+        int[][] test = {};
+        test[0][0] = 1;
+    }
+}
+
+class SumArray {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = arr1;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] += arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
