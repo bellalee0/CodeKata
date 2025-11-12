cd755c0a37d3b78fe6455c7aed102362760b3e82
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-12 10:24:42 +0900

    11.12(수) _ 알고리즘 #39 코드카타(수정)

--- Algorithm_Programmers/최대공약수와_최소공배수.java
+++ Algorithm_Programmers/최대공약수와_최소공배수.java
@@ -33,6 +33,7 @@ class CommonDivisorCommonMultiple {
         int[] answer = new int[2];
         answer[0] = gcd(n, m);
         answer[1] = n * m /  answer[0];
+        return answer;
     }
 
     public int gcd(int a, int b) {
