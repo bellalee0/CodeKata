05d33383fa15caa8d851051d52924e473b4486d1
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-11-26 09:25:05 +0900

    11.26(수) _ SQL #57 코드카타

---
+++ SQL_Programmers/String, Date _ 조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164672
+-- USED_GOODS_BOARD 테이블에서 2022년 10월 5일에 등록된 중고거래 게시물의 게시글 ID, 작성자 ID, 게시글 제목, 가격, 거래상태를 조회하는 SQL문을 작성해주세요. 거래상태가 SALE 이면 판매중, RESERVED이면 예약중, DONE이면 거래완료 분류하여 출력해주시고, 결과는 게시글 ID를 기준으로 내림차순 정렬해주세요.
+
+select board_id, writer_id, title, price,
+       (case when status = 'SALE' then '판매중'
+             when status = 'RESERVED' then '예약중'
+             else '거래완료'
+           end) as status
+from used_goods_board
+where date_format(created_date, '%Y-%m-%d') = '2022-10-05'
+order by 1 desc
