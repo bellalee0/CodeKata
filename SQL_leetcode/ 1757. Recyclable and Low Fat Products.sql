190e3f5d3a38ca9e1864276b9598054017d9c6b6
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-30 09:18:37 +0900

    12.30(화) _ SQL #77 코드카타

---
+++ SQL_leetcode/ 1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/description/
+-- Write a solution to find the ids of products that are both low fat and recyclable. Return the result table in any order. The result format is in the following example.
+
+select product_id
+from products
+where low_fats = 'Y' and recyclable = 'Y'
