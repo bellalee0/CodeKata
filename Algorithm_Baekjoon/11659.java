8f3d31a8a283eeabd247190c4d6d99558d41b7af
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-04-16 11:28:37 +0900

    04.16(목) _ 알고리즘 백준 #11659 풀이 변경

--- Algorithm_Baekjoon/11659.java
+++ Algorithm_Baekjoon/11659.java
@@ -1,32 +1,40 @@
 // 11659 - 구간 합 구하기
 // 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지의 합 구하기
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int size = scanner.nextInt();
-        int times = scanner.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+        int times = Integer.parseInt(st.nextToken());
 
         StringBuilder result = new StringBuilder();
 
         int[] sumArray = new int[size+1];
 
+        st = new StringTokenizer(br.readLine());
+
         for (int i = 1; i <= size; i++) {
             
-            int num = scanner.nextInt();
+            int num = Integer.parseInt(st.nextToken());
 
             sumArray[i] = sumArray[i-1] + num;
         }
 
         for (int i = 0; i < times; i++) {
 
-            int start = scanner.nextInt();
-            int end = scanner.nextInt();
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
             int answer = sumArray[end] - sumArray[start-1];
 
@@ -34,7 +42,5 @@ class Main {
         }
 
         System.out.println(result);
-
-        scanner.close();
     }
 }
